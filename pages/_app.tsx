0150b91582f32cc0c1f4f823b7b1a3d07617c581
Abdul-Kareem Abdul-Fattah <adeoyin2828@gmail.com>
2021-10-16 23:45:05 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,10 +8,10 @@ function MyApp({ Component, pageProps, router }) {
   return (
     <ThemeProvider attribute='class'>
   <div className="grid grid-cols-12 gap-6 px-5 lg:px-48 my-14 sm:px-20 md:px-32">
-      <div className='col-span-12 p-4 text-center bg-white dark:bg-dark-500 lg:col-span-3 col rounded-2xl shadow-custom-light dark:shadow-custom-dark'>
+      <div className='col-span-12 p-4 text-center  bg-dark-500 lg:col-span-3 col rounded-2xl shadow-custom-dark'>
         <Sidebar/>
       </div>
-      <div className='flex flex-col col-span-12 overflow-hidden bg-white lg:col-span-9 rounded-2xl dark:bg-dark-500 shadow-custom-light dark:shadow-custom-dark'>
+      <div className='flex flex-col col-span-12 overflow-hidden lg:col-span-9 rounded-2xl bg-dark-500 shadow-custom-dark'>
         <Navbar/>
         <AnimatePresence exitBeforeEnter>
            <Component {...pageProps} key={router.route}/>
