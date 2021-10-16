84aecb878a189421842ba472793d832c028aa0d4
Abdul-Kareem Abdul-Fattah <adeoyin2828@gmail.com>
2021-10-17 00:01:53 +0100

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,7 +12,7 @@ class MyDocument extends Document {
 <link rel="preconnect" href="https://fonts.gstatic.com" />
 <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet" />
         </Head>
-        <body className="bg-fixed bg-gradient-to-r from-green to-blue-500 dark:from-dark-500 dark:to-dark-700 dark:text-white">
+        <body className="bg-fixed bg-gradient-to-r from-dark-500 to-dark-700 text-white">
           <Main />
           <NextScript />
         </body>
