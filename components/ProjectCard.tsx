25255447d4f9755a51c281bb7e4697f4b74a3d3c
Abdul-Kareem Abdul-Fattah <adeoyin2828@gmail.com>
2021-10-16 23:55:13 +0100

    Update ProjectCard.tsx

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -37,7 +37,7 @@ setShowDetail: (id:null|number) => void
             <p className="my-2 text-center">{name}</p>
               
             {showDetail === id && (
-        <motion.div variants={stagger} initial="initial" animate="animate" className="absolute top-0 left-0 z-10 grid w-full h-auto p-2 text-black bg-gray-100 rounded-lg md:p-10 md:grid-cols-2 gap-x-12 dark:text-white dark:bg-dark-100">
+        <motion.div variants={stagger} initial="initial" animate="animate" className="absolute top-0 left-0 z-10 grid w-full h-auto p-2 text-black rounded-lg md:p-10 md:grid-cols-2 gap-x-12 text-white bg-dark-100">
           <div>
             {/* <img src={image_path} alt={name} /> */}
                 <motion.div variants={fadeInUp} className='border-4 border-gray-100'>
@@ -53,13 +53,13 @@ setShowDetail: (id:null|number) => void
             <motion.div className="flex justify-center my-4 space-x-3">
               <a
                 href={github_url}
-                className="flex items-center px-4 py-2 space-x-3 text-lg bg-gray-200 dark:bg-dark-200"
+                className="flex items-center px-4 py-2 space-x-3 text-lg bg-dark-200"
               >
                 <AiFillGithub /> <span>Github</span>
               </a>
               <a
                 href={deployed_url}
-                className="flex items-center px-4 py-2 space-x-3 text-lg bg-gray-200 dark:bg-dark-200"
+                className="flex items-center px-4 py-2 space-x-3 text-lg bg-dark-200"
               >
                 <AiFillProject /> <span>Project</span>
               </a>
@@ -74,7 +74,7 @@ setShowDetail: (id:null|number) => void
               {key_techs.map((tech) => (
                 <span
                   key={tech}
-                  className="px-2 py-1 my-1 bg-gray-200 dark:bg-dark-200 rounde-sm"
+                  className="px-2 py-1 my-1 bg-dark-200 rounde-sm"
                 >
                   {tech}
                 </span>
@@ -84,7 +84,7 @@ setShowDetail: (id:null|number) => void
 
           <button
             onClick={() => setShowDetail(null)}
-            className="absolute p-1 bg-gray-200 rounded-full top-3 right-3 focus:outline-none dark:bg-dark-200"
+            className="absolute p-1  rounded-full top-3 right-3 focus:outline-none bg-dark-200"
           >
             <MdClose size={30} />
           </button>
